033016abfa83c34d87d897f71a617fc17bfb7ac2
Lyndon White <lyndon.white@invenialabs.co.uk>
2019-01-25 20:01:24 +0000

    make assigment capturing work

--- src/detailed.jl
+++ src/detailed.jl
@@ -1,19 +1,18 @@
-
-
-
-
 Cassette.@context HandEvalCtx
 
 
-#function Cassette.overdub(ctx::HandEvalCtx, f, args...)
-#end
-
-function Cassette.overdub(ctx::HandEvalCtx, f, callback, args...)
+function Cassette.overdub(ctx::HandEvalCtx, f, args...)
     if Cassette.canrecurse(ctx, f, args...)
-        _ctx = Cassette.similarcontext(ctx, metadata = callback)
-        return Cassette.recurse(_ctx, f, args...) # return result, callback
+        _ctx = Cassette.similarcontext(ctx, metadata = Dict())
+        
+        try
+            return Cassette.recurse(_ctx, f, args...)
+        finally
+            # save assignments from functions
+            ctx.metadata[gensym(nameof(f))] = _ctx.metadata
+        end
     else
-        return Cassette.fallback(ctx, f, args...), callback
+        return Cassette.fallback(ctx, f, args...)
     end
 end
 
@@ -31,31 +30,29 @@ function map_assignments(::Type{<:HandEvalCtx}, reflection::Cassette.Reflection)
     getmetadata = Expr(:call, Expr(:nooverdub, GlobalRef(Core, :getfield)), Expr(:contextslot), QuoteNode(:metadata))
 
     # insert the initial `metadata slot` assignment into the IR.
-    Cassette.insert_statements!(ir.code, ir.codelocs,
-                                (stmt, i) -> i == 1 ? 2 : nothing,
-                                (stmt, i) -> [Expr(:(=), metadata_slot, getmetadata), stmt])
+    Cassette.insert_statements!(
+        ir.code, ir.codelocs,
+        (stmt, i) -> i == 1 ? 2 : nothing,
+        (stmt, i) -> [Expr(:(=), metadata_slot, getmetadata), stmt]
+    )
 
-    
+
     # Now the real part where we determine about assigments
     # What we want to do is:
     # After every assigment: `x = foo`, insert `ctx.metadata[:x] = x`
-
+    # Skip first line where we store Metadata
     is_assignment(stmt) = Base.Meta.isexpr(stmt, :(=))
-    stmtcount(stmt, i) = is_assignment(stmt) ? 2 : nothing
+    stmtcount(stmt, i) = i > 1 && is_assignment(stmt) ? 2 : nothing
     
     function newstmts(stmt, i)
         lhs = stmt.args[1]
         name = slotname(ir, lhs)
-        @show name
         record = Expr(
-            :(=),
-            Expr(
-                :call,
-                Expr(:nooverdub, GlobalRef(Base, :setindex!)),
-                metadata_slot,
-                lhs,
-                name
-            )
+            :call,
+            Expr(:nooverdub, GlobalRef(Base, :setindex!)),
+            metadata_slot,
+            lhs,
+            QuoteNode(name)
         )
         
         return [stmt, record]
