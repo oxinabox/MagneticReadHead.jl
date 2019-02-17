2cb753f9eb31da98282881b088c376e1dff730e3
Lyndon White <lyndon.white@invenialabs.co.uk>
2019-02-17 20:57:13 +0000

    correct debug

--- test/test_behavour.jl
+++ test/test_behavour.jl
@@ -10,10 +10,10 @@ end
 
 @testset "Should not block throwing errors" begin
     foo() = throw(DemoException())
-    @test_throws(DemoException, (@debug foo()))
+    @test_throws(DemoException, (@iron_debug foo()))
     
     
     bar() = map(_->foo(), 1:10)
-    @test_throws(DemoException, (@debug foo()))
+    @test_throws(DemoException, (@iron_debug foo()))
 
 end
