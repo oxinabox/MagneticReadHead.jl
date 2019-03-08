0bfa35669fd392dfe21374e28f695b9e4de0c4c7
Lyndon White <lyndon.white@invenialabs.co.uk>
2019-03-08 15:05:53 -0800

    rewrite to use CodeTracking.jl

--- src/locate.jl
+++ src/locate.jl
@@ -1,86 +1,36 @@
 """
-    pkgdata(mod)
+    source_paths(mod, file)
 
-Gets all the data Revise has on the given module.
+Returns list of all source files in the module with matching (partial) filename,
+the filename does not have to be relative to the base directory.
+It can be any partial path.
+Ambiguities can occur as 
+if no matching path is found then an empty list is returned
 """
-pkgdata(pkg_id::Base.PkgId) = Revise.pkgdatas[pkg_id]
-pkgdata(mod::Module) = pkgdata(Base.PkgId(mod))
-
-"""
-    filemap(mod, file)
-
-Retrieve all the data Revise has on the contents of given file
-from the given module.
-"""
-function filemap(mod, file)
-    mdata = pkgdata(mod)
-    mfiles = joinpath.(mdata.path, keys(mdata.fileinfos))
+function source_paths(mod, file)
+    mdata = pkgfiles(mod)
+    mfiles = joinpath.(mdata.basedir, mdata.files)
     
     file = expanduser(file)
     if !isabspath(file)
         # We do not need it to be absolute
-        # But we do want it to start with a "/"
+        # But we do want it to start with a "/" if it isn't
         # So we can use `endswith` to check that it matchs one of the paths
         # that we know about, without worry of matching part of a filename
         file = "/" * file
     end
-    matched_ind = findfirst(mfile->endswith(mfile, file) , mfiles)
-    matched_ind ===  nothing && return nothing
+    matched_inds = map(mfile->endswith(mfile, file), mfiles)
      
-    internal_file = collect(keys(mdata.fileinfos))[matched_ind]
-    Revise.maybe_parse_from_cache!(mdata, internal_file)  # Ensure fileinfo filled
-    finfo = collect(values(mdata.fileinfos))[matched_ind]
-    return finfo.fm
+    return mfiles[matched_inds]
 end
-
-######################################################################
-# These come from Rebugger.jl
-# Including them here as Rebugger itself is causing problems.
-# They can away once code tracking gets a bit more stuff.
-# https://github.com/timholy/CodeTracking.jl/issues/3
-
-using Revise: ExLike
 
 
 """
-    r = linerange(expr, offset=0)
-Compute the range of lines occupied by `expr`.
-Returns `nothing` if no line statements can be found.
-"""
-function linerange(def::ExLike, offset=0)
-    start, haslinestart = findline(def, identity)
-    stop, haslinestop  = findline(def, Iterators.reverse)
-    (haslinestart & haslinestop) && return (start+offset):(stop+offset)
-    return nothing
-end
+    containing_methods([module], filename, linenum)
 
-function findline(ex, order)
-    ex.head == :line && return ex.args[1], true
-    for a in order(ex.args)
-        a isa LineNumberNode && return a.line, true
-        if a isa ExLike
-            ln, hasline = findline(a, order)
-            hasline && return ln, true
-        end
-    end
-    return 0, false
-end
-
-
-function linerange((def, (sig, offset))::Tuple{Any, Tuple{Any, Int}})
-    return linerange(def, offset)
-end
-
-# This is not a function so just return an empty range
-linerange((def, none)::Tuple{Any, Nothing}) = 1:0
-
-
-########################################################################
-"""
-    containing_method([module], filename, linenum)
-
-Returns the method within which that line number, in that file, occurs.
-Returns `nothing` on no match.
+Returns the methods within which the provided line number
+in the given file, occurs.
+Returns an empty list on no match.
 
 If the module is not provided, then all modules loaded
 will be searched for a file with that name that has a function over that line.
@@ -89,35 +39,33 @@ Filenames can be absolute, or partial.
 E.g. `/home/user1/dev/MyMod/src/helpers/utils.jl`,
 `helpers/utils.jl` or `utils.jl` are all acceptable.
 
-However, if multiple files match the module (or lack of module),
-and filename specification then only one will be  selected.
+If it is ambigious, then all matching methods in all file will be returned.
 """
-function containing_method(mod, file, linenum)
-    module_fmaps = filemap(mod, file)
-    module_fmaps === nothing && return nothing
-    for (inner_mod, fmaps) in module_fmaps
-        for entry in fmaps.defmap
-            def, info = entry
-            lr = linerange((def, info))
-            # TODO: workout  a way to round-forward as the linerange starts from first
-            # statement within the functions body, not from the line it is "declared"
-            # And there could be quiet some whitespace
-            if linenum ∈ lr
-                sigt, offset = info
-                return sigt2meth(sigt[end])
-            end
-        end
+function containing_methods(mod, file, linenum)
+    paths = source_paths(mod, file)
+    isempty(paths) && return Method[]
+    sigs = mapreduce(vcat, paths) do path
+        signatures_at(path, linenum)
+    end
+    # TODO: workout  a way to round-forward as the signatures_at
+    # only matches to
+    # statement within the functions body, not from the line it is "declared"
+    # or any intervening whitespace. It also doesn't match to the `end` line.
+    
+    return map(sigt2meth, sigs)
+end
+
+function containing_methods(file, linenum)
+    #TODO: raise issue on CodeTracking.jl to expose public way
+    # to get list of all loaded modules
+    pkgids = keys(CodeTracking._pkgfiles)
+    isempty(pkgids) && return Method[]
+    mapreduce(vcat, pkgids) do pkgid
+        containing_methods(pkgid, file, linenum)
     end
 end
 
-function containing_method(file, linenum)
-    for pkg_id in keys(Revise.pkgdatas)
-        meth = containing_method(pkg_id, file, linenum)
-        meth !== nothing && return meth
-    end
-end
-
-function sigt2meth(::Type{SIGT}) where SIGT
+function sigt2meth(::Type{SIGT})::Method where SIGT
     params = SIGT.parameters
     func_t = params[1]
     func = func_t.instance
--- test/test_locate.jl
+++ test/test_locate.jl
@@ -3,9 +3,9 @@ using MagneticReadHead
 using Test
 
 using MagneticReadHead:
-    filemap, pkgdata, containing_method, src_line2ir_statement_ind
-
-@show Revise.pkgdatas #BLACKMAGIC: Remove this line and the tests fail
+    source_paths,
+    containing_methods,
+    src_line2ir_statement_ind
 
 @testset "src_line2ir_statement_ind" begin
     ir1line = first(methods(()->1)) |> Base.uncompressed_ast
@@ -21,30 +21,24 @@ using MagneticReadHead:
     @test src_line2ir_statement_ind(ir2line, (@__LINE__)-1) == 3
 end
 
+@testset "source_paths" begin
+    @test source_paths(MagneticReadHead, "utils.jl") |> !isempty
+    @test source_paths(MagneticReadHead, "src/utils.jl") == source_paths(MagneticReadHead, "utils.jl")
 
-@testset "pkgdata" begin
-    @test pkgdata(MagneticReadHead) !==nothing
+    @test source_paths(MagneticReadHead, "NOT_REAL") |> isempty
 end
 
-@testset "filemap" begin
-    @test filemap(MagneticReadHead, "utils.jl") !==nothing
-    @test filemap(MagneticReadHead, "src/utils.jl") == filemap(MagneticReadHead, "utils.jl")
-
-    @test filemap(MagneticReadHead, "NOT_REAL") ===nothing
-end
-
-
-@testset "containing_method" begin
-    
-    meth = containing_method(MagneticReadHead, "src/locate.jl", 30)
-    for ln in (29, 30, 31)
-        @test meth == containing_method(MagneticReadHead, "src/locate.jl", ln)
-        @test meth == containing_method(MagneticReadHead, "locate.jl", ln)
-        @test meth == containing_method("locate.jl", ln)
+@testset "containing_methods" begin
+    # BADTEST: This is actually tied to line numbers in the source code
+    meth = containing_methods(MagneticReadHead, "src/locate.jl", 46)
+    for ln in (45, 46, 47)
+        @test meth == containing_methods(MagneticReadHead, "src/locate.jl", ln)
+        @test meth == containing_methods(MagneticReadHead, "locate.jl", ln)
+        @test meth == containing_methods("locate.jl", ln)
         @test_broken meth ==
-            containing_method(MagneticReadHead, "../src/locate.jl", ln)
+            containing_methods(MagneticReadHead, "../src/locate.jl", ln)
         cd(@__DIR__) do
-            @test meth == containing_method(
+            @test meth == containing_methods(
                 MagneticReadHead,
                 realpath("../src/locate.jl"),
                 ln
@@ -52,4 +46,3 @@ end
         end
     end
 end
-
