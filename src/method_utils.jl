e40328ca4d1aac45686fecc2a02e6057895e9177
Lyndon White <lyndon.white@invenialabs.co.uk>
2019-04-19 20:52:12 +0100

    expand method_utils. Now we have functionality for checking dispatchs, and also now moduleof returns the module of a methods function, not the methods module

--- src/method_utils.jl
+++ src/method_utils.jl
@@ -1,22 +1,44 @@
-moduleof(m::Method) = m.module
+"""
+    moduleof
+
+Returns the module associated with a method.
+Note: this is not the module where it was defined (m.module)
+but rather the module for which it's function was defined.
+"""
+moduleof(m::Method) = functiontypeof(m).name.module
 
 functiontypeof(m::Method) = parameter_typeof(m.sig)[1]
 parameter_typeof(sig::UnionAll) = parameter_typeof(sig.body)
 parameter_typeof(sig::DataType) = sig.parameters
 
-function methodof(@nospecialize(f), @nospecialize(args...))
-    try
-        @which(f(args...))
-    catch
-        @warn "Determining methods failed" f arg_types=typeof.(args)
-        rethrow()
-    end
+"""
+    DispatchAttempt
+This is a lightweight type that represents a attemopt to dispatch against a method.
+It has two type params, `F` the function type,
+and `A` a tuple type of arguments.
+"""
+struct DispatchAttempt{F,A}
+    f::F
+    args::A
 end
 
-methodof(::Core.Builtin, args...) = nothing  # No methods for `Builtin`s
+"""
+    _typeof
+Like `typeof` but when applied to `DataType`s, returns `Type{T}`, rather than `DataType`
+"""
+_typeof(x)=typeof(x)
+_typeof(::Type{T}) where T = Type{T}
 
-#TODO: Methods are actually pretty slow to construct. 1-2μs and 8-15 allocations
-# when we construct them ourself we are only really using them has a container for
-# function, args types and module.
-# We could define out own `LightMethod`, which exposes also overloads the above
-# functions, while still keeping them working on real methods. And then just use that
+"""
+    accepts(meth::Method, ::DispatchAttempt{F,A}) where {F,A}
+It can be compared against a `Method` to see if that method would
+accept this dispatch.
+It does not check if that is the highest priority method that would accept this dispatch
+"""
+function accepts(meth::Method, da::DispatchAttempt)
+    dispatch_sig = Tuple{_typeof(da.f), map(_typeof, da.args)...}
+    return dispatch_sig <: meth.sig
+end
+
+# Builtin's do not have methods
+accepts(::Method, ::DispatchAttempt{<:Core.Builtin}) = false
--- test/test_method_utils.jl
+++ test/test_method_utils.jl
@@ -1,9 +1,20 @@
 using Test
-using MagneticReadHead: moduleof, functiontypeof, methodof
+using InteractiveUtils
+using MagneticReadHead: moduleof, functiontypeof, DispatchAttempt, accepts
+
+
+# Define an extra method of eps in this module, so we can test methods of
+Base.eps(::typeof(moduleof)) = "dummy"
 
 @testset "moduleof" begin
     for meth in methods(detect_ambiguities)
         @test moduleof(meth) == Test
+    end
+
+    # We define a verion of eps in this module
+    # but we expect that it is still counted as being in `Base`
+    for meth in methods(eps)
+        @test moduleof(meth) == Base
     end
 end
 
@@ -20,33 +31,47 @@ end
     end
 end
 
-@testset "methodof" begin
+@testset "DispatchAttempt" begin
     @testset "BuiltIns" begin
-        @test methodof(Core.typeof) === nothing
-        @test methodof(Core.typeof, 1) === nothing
-    end
-
-    @testset "Ensure no errors" begin
-       @test methodof(+, 1, 1) isa Method
-       @test methodof(+, 2, 2.0) isa Method
-       @test methodof(+, 2.0, 2 + im) isa Method
-       
-       @test methodof(eps) isa Method
-       @test methodof(eps, Float32) isa Method
-    end
-
-    @testset "local function no args" begin
-        bar() = 1
-        @test functiontypeof(methodof(bar)) == typeof(bar)
-        @test moduleof(methodof(bar)) == @__MODULE__
+        meth_not = @which(!true)
+        @test accepts(meth_not, DispatchAttempt(Core.typeof, tuple())) === false
+        @test accepts(meth_not, DispatchAttempt(Core.typeof, (1,))) === false
     end
 
 
-    @testset "local function 1 arg" begin
-        foo(::Int) = 1
-        @test functiontypeof(methodof(foo, 20)) == typeof(foo)
-        @test moduleof(methodof(foo, 20)) == @__MODULE__
+    @testset "accepts" begin
+        function method_accepts(meth, f, args...)
+            da = DispatchAttempt(f, args)
+            return accepts(meth, da)
+        end
+        function check_accepts(f, args...)
+            default_meth = @which f(args...)
+            return method_accepts(default_meth, f, args...)
+        end
+
+        @test check_accepts(+, 1, 1)
+        @test check_accepts(+, 2, 2.0)
+        @test check_accepts(+, 2.0, 2 + im)
+        @test !method_accepts((@which 1+1), +, 'a', 'b')  # wrong method
+        @test !method_accepts((@which 1+1), *, 'a', 'b')  # wrong method
+
+        @test check_accepts(eps)  # No argument
+        @test check_accepts(eps, Float32)  #DataType as argument
+        @test !method_accepts((@which eps(Float32)), Int64) # wrong method
+
+        @testset "Constructor" begin
+            @test check_accepts(Int, true)
+            @test check_accepts(Int, 'c')
+            @test !method_accepts((@which Int(true)), 'c') # wrong method
+        end
+
+        @testset "Closure" begin
+            bar = 24
+            foo(x::Int) = x*bar
+            @test check_accepts(foo, 5)
+            @test !method_accepts((@which foo(5)), foo, 'c')
+        end
     end
 
-
+    
 end
